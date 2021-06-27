ed8a7461bb36b4eaaba2d514f5b2e140c8985456
AndaisRin <32798810+AndaisRin@users.noreply.github.com>
2021-06-27 17:26:30 +0300

    Update solution01.py

--- Coursera 2 week/solution01.py
+++ Coursera 2 week/solution01.py
@@ -1,5 +1,6 @@
-chis1 = int(input())
-chis2 = int(input())
+#Напишите программу, которая считывает два целых числа A и B и выводит наибольшее значение из них.
+
+chis1, chis2= int(input()), int(input())
 if chis1 > chis2:
     print(chis1)
 else:
