8c5e79da966b6726abe19fc1a681e5c73418e89c
AndaisRin <32798810+AndaisRin@users.noreply.github.com>
2021-06-28 20:14:51 +0300

    Update solution17.py

--- Coursera 2 week/solution17.py
+++ Coursera 2 week/solution17.py
@@ -1,8 +1,9 @@
-a = int(input())
-b = int(input())
-c = int(input())
-d = int(input())
-e = int(input())
+#За многие годы заточения узник замка Иф проделал в стене прямоугольное отверстие размером D×E.
+#Замок Иф сложен из кирпичей, размером A×B×C. 
+#Определите, сможет ли узник выбрасывать кирпичи в море через это отверстие.
+
+a, b, c, d, e = int(input()), int(input()), int(input()), int(input()), int(input())
+
 if min(a, b, c) * (a + b + c - min(a, b, c) - max(a, b, c) <= d * e) and min(
         a, b, c) <= min(d, e) and (
         a + b + c - min(a, b, c) - max(a, b, c)) <= d + e - min(d, e):
