d1a543e6d576b396efa35d0e69bff2cb5afc79e1
AndaisRin <32798810+AndaisRin@users.noreply.github.com>
2021-06-28 21:17:45 +0300

    Update solution32.py

--- Coursera 2 week/solution32.py
+++ Coursera 2 week/solution32.py
@@ -1,7 +1,9 @@
-a = int(input())
+#По данному натуральному n вычислите сумму 1²+2²+3²+...+n².
+
+n = int(input())
 sum = 0
 i = 0
-while i <= a:
+while i <= n:
     sum = sum + i ** 2
     i = i + 1
 print(sum)
