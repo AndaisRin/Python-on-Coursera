20bdb14fd34c6620b147ae618f589c6d6595afdf
AndaisRin <32798810+AndaisRin@users.noreply.github.com>
2021-06-28 17:53:57 +0300

    Update solution14.py

--- Coursera 2 week/solution14.py
+++ Coursera 2 week/solution14.py
@@ -1,6 +1,7 @@
-a = int(input())
-b = int(input())
-c = int(input())
+#Даны три целых числа A, B, C. 
+#Определить, есть ли среди них хотя бы одно четное и хотя бы одно нечетное.
+
+a, b, c = int(input()), int(input()), int(input())
 if (a % 2 == 0 or b % 2 == 0 or c % 2 == 0) and (
         a % 2 != 0 or b % 2 != 0 or c % 2 != 0):
     print('YES')
