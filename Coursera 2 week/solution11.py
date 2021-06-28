cf9c427029c6b99fd634d472663ba27851f9fc1e
AndaisRin <32798810+AndaisRin@users.noreply.github.com>
2021-06-28 15:33:55 +0300

    Update solution11.py

--- Coursera 2 week/solution11.py
+++ Coursera 2 week/solution11.py
@@ -1,7 +1,7 @@
-x1 = int(input())
-y1 = int(input())
-x2 = int(input())
-y2 = int(input())
+#Даны координаты двух точек на плоскости, требуется определить, 
+# лежат ли они в одной координатной четверти или нет (все координаты отличны от нуля).
+
+x1, y1, x2, y2 = int(input()), int(input()), int(input()), int(input())
 if x1 * x2 / abs(x1 * x2) == 1 and y1 * y2 / abs(y1 * y2) == 1:
     print('YES')
 else:
