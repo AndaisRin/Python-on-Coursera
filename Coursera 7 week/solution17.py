9a1ed5eb5ed835f459dcca90f804aa9b4217552a
AndaisRin <32798810+AndaisRin@users.noreply.github.com>
2021-10-15 01:22:20 +0300

    Add files via upload

---
+++ Coursera 7 week/solution17.py
@@ -0,0 +1,17 @@
+#
+count_words = dict()
+data = open('input.txt', 'r', encoding='utf8')
+for line in data:
+    words = list(map(str, line.split()))
+    for i in range(len(words)):
+        if words[i] not in count_words.keys():
+            count_words[words[i]] = 1
+        else:
+            count_words[words[i]] += 1
+max_count = max(count_words.values())
+max_word = []
+for key, value in count_words.items():
+    if value == max_count:
+        max_word.append(key)
+max_word.sort()
+print(max_word[0])
